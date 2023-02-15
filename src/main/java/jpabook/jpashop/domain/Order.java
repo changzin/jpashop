b9b8160af9735c5fd2aff1191aebbc78c1195e0a
Me <beolle99@gmail.com>
2023-02-16 07:43:50 +0900

    My Jpa Study

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+    private LocalDateTime orderDate;
+    @Enumerated(EnumType.STRING)
+    private Orderstatus status;
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Orderstatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(Orderstatus status) {
+        this.status = status;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void newOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+    }
+}
